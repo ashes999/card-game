fc10ae417d2ff1d8f0191b5e314ecd9886c5c5d0
ashes999 <ashes999@yahoo.com>
2015-02-15 17:16:16 -0500

    Show five cards on player's side

--- code/source/PlayState.hx
+++ code/source/PlayState.hx
@@ -6,22 +6,38 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
-
+import flash.Lib;
 /**
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState
 {
 	/**
-	 * Function that is called up when to state is created to set it up. 
+	 * Function that is called up when to state is created to set it up.
 	 */
 	override public function create():Void
 	{
 		super.create();
+		this.addAndShow('assets/images/background.png');
+
+		// Five of yours
+		for(n in 0...5) {
+			var card = this.addAndShow('assets/images/sample-card.png');
+			card.x = (n * card.width) + ((n+1) * 16);
+			card.y = Main.gameHeight - card.height -  16;
+		}
 	}
-	
+
+	private function addAndShow(string:String)
+	{
+		var s:FlxSprite = new FlxSprite();
+		s.loadGraphic(string);
+		add(s);
+		return s;
+	}
+
 	/**
-	 * Function that is called when this state is destroyed - you might want to 
+	 * Function that is called when this state is destroyed - you might want to
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
 	override public function destroy():Void
@@ -35,5 +51,5 @@ class PlayState extends FlxState
 	override public function update():Void
 	{
 		super.update();
-	}	
+	}
 }
